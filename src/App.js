3ac02d02eb09f4886988598cf596c58632b1c9c5
michaelayegbamartins4real@gmail.com <michaelayegbamartins4real@gmail.com>
2023-12-18 08:39:32 +0100

    playing-with-accordions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordion from "./Accordion";
+import { accordionData } from "./utils/Content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => {
+          return <Accordion title={title} content={content} />;
+        })}
+      </div>
     </div>
   );
 }
